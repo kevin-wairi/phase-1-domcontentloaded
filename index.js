a5149c0d5c8fa17805f23b3dba45a767e3e8c223
kevin Wairi <kevinwairi@gmail.com>
2023-03-24 16:43:29 +0300

    Assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded',function(e){
+    console.log('DOM has Loaded')
+    let txt = document.getElementById('text')
+    txt.innerHTML = "This is really cool!";
+})
+console.log('The DOM is Loaded');
